7e32a8b33f9f3f7101fd94267f5de4740d115517
Mykola <kolia.chaun@icloud.com>
2024-04-07 21:17:41 +0300

    create user model

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            token_obtain_pair)
 
 from issues.api import create_new_issue, get_issue
+from users.api import create_user
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("users/", create_user),
     path("create-issue", create_new_issue),
     path("get-issue", get_issue),
+    # Authentication
+    # path("auth/token/", token_obtain_pair),
+    path("auth/token/", TokenObtainPairView.as_view()),
 ]
