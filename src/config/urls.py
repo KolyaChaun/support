fc77e73d0ed3a13accba2596967327bbe9bdceb2
Mykola Chaiun <109896908+KolyaChaun@users.noreply.github.com>
2024-04-19 21:17:32 +0300

    HTTP operations for Users (#5)

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from issues.api import create_new_issue, get_issue
-from users.api import create_user
+from issues.api import IssuesAPI, IssuesRetrieveUpdateDeleteAPI
+from users.api import UserCreateAPI, UserUpdateAPI
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("users/", create_user),
-    path("create-issue", create_new_issue),
-    path("get-issue", get_issue),
+    path("users/", UserCreateAPI.as_view()),
+    path("users/<int:id>", UserUpdateAPI.as_view()),
+    path("issues/", IssuesAPI.as_view()),
+    path("issues/<int:id>", IssuesRetrieveUpdateDeleteAPI.as_view()),
     # Authentication
     # path("auth/token/", token_obtain_pair),
     path("auth/token/", TokenObtainPairView.as_view()),
--- src/users/api.py
+++ src/users/api.py
@@ -1,26 +1,85 @@
-import json
-
 from django.contrib.auth import get_user_model
-from django.http import HttpRequest, JsonResponse
+from django.contrib.auth.hashers import make_password
+from rest_framework import generics, permissions, serializers, status
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+
+from users.enums import Role
 
 User = get_user_model()
 
 
-def create_user(request: HttpRequest) -> JsonResponse:
-    if request.method != "POST":
-        raise NotImplementedError("Only POST requests")
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
 
-    data: dict = json.loads(request.body)
-    user = User.objects.create_user(**data)
 
-    # convert to dict
-    results = {
-        "id": user.id,
-        "email": user.email,
-        "first_name": user.first_name,
-        "last_name": user.last_name,
-        "role": user.role,
-        "is_active": user.is_active,
-    }
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["email", "password", "first_name", "last_name", "role"]
 
-    return JsonResponse(results)
+    def validate(self, attrs: dict) -> dict:
+        attrs["password"] = make_password(attrs["password"])
+        return attrs
+
+    def validate_role(self, value: str) -> str:
+        if value not in Role.users():
+            raise ValidationError(
+                f"Selected Role must be in {Role.users_values()}",
+            )
+        return value
+
+
+class UserCreatePublicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["email", "first_name", "last_name", "role"]
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "first_name", "last_name"]
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.save()
+        return instance
+
+
+class UserCreateAPI(generics.CreateAPIView):
+    http_method_names = ["post"]
+    serializer_class = UserCreateSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        return User.objects.all()
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        return Response(
+            UserCreatePublicSerializer(serializer.validated_data).data,
+            status=status.HTTP_201_CREATED,
+            headers=self.get_success_headers(serializer.data),
+        )
+
+
+class UserUpdateAPI(generics.RetrieveUpdateAPIView):
+    http_method_names = ["get", "put"]
+    serializer_class = UserUpdateSerializer
+    permission_classes = [permissions.AllowAny]
+    lookup_url_kwarg = "id"
+
+    def get_queryset(self):
+        return User.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            return UserSerializer
+        return UserUpdateSerializer
