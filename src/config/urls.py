739203662d3043585ad29be407b41c6ec9c7e2c0
Mykola <kolia.chaun@icloud.com>
2024-04-24 19:10:15 +0300

    api views for issues and messages

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from issues.api import IssuesAPI, IssuesRetrieveUpdateDeleteAPI
+from issues.api import (IssuesAPI, IssuesRetrieveUpdateDeleteAPI, issues_close,
+                        issues_take, messages_api_dispatcher)
 from users.api import UserCreateAPI, UserDestroyAPI, UserRetrieveUpdateAPI
 
 urlpatterns = [
@@ -14,6 +15,10 @@ urlpatterns = [
     # issues
     path("issues/", IssuesAPI.as_view()),
     path("issues/<int:id>", IssuesRetrieveUpdateDeleteAPI.as_view()),
+    path("issues/<int:id>/close", issues_close),
+    path("issues/<int:id>/take", issues_take),
+    # messages
+    path("issues/<int:issue_id>/messages", messages_api_dispatcher),
     # Authentication
     # path("auth/token/", token_obtain_pair),
     path("auth/token/", TokenObtainPairView.as_view()),
